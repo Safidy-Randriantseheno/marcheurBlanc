d0525b43bf14c40158c0aa883d72343322d7e9bb
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-18 09:05:37 +0300

    feat: initial project .

---
+++ src/main/java/com/projet1/marcheureBlanc/MarcheureBlancApplication.java
@@ -0,0 +1,13 @@
+package com.projet1.marcheureBlanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcheureBlancApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcheureBlancApplication.class, args);
+	}
+
+}
