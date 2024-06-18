63134854e573bde223f786852def142172f69a1e
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-06-18 10:27:31 +0300

    feat: create test testPointDeDepartInchange

--- src/test/java/com/projet1/marcheureBlanc/MarcheurTest.java
+++ src/test/java/com/projet1/marcheureBlanc/MarcheurTest.java
@@ -83,4 +83,39 @@ public class MarcheurTest {
 
         marcheur.afficherTrajectoire(parcours);
     }
+    @Test
+    public void testPointDeDepartInchange() {
+        Lieu lieu1 = new Lieu("hei", null);
+        Lieu lieu2 = new Lieu("Sekolintsika", null);
+        Lieu lieu3 = new Lieu("Marais", null);
+        Lieu lieu4 = new Lieu("Pullman", null);
+        Lieu lieu5 = new Lieu("Balançoire", null);
+        Lieu lieu6 = new Lieu("Boulevard", null);
+        Lieu destination = new Lieu("Esti", null);
+
+        Route route1 = new Route("Route 1");
+        Route route2 = new Route("Route 2");
+        Route route3 = new Route("Route 3");
+        Route route4 = new Route("Route 4");
+        Route route5 = new Route("Route 5");
+        Route route6 = new Route("Route 6");
+
+        lieu1.setRoutesAdjacentes(Arrays.asList(route1, route2));
+        lieu2.setRoutesAdjacentes(Arrays.asList(route1, route3, route4));
+        lieu3.setRoutesAdjacentes(Arrays.asList(route2, route4, route5));
+        lieu4.setRoutesAdjacentes(Arrays.asList(route3, route6));
+        lieu5.setRoutesAdjacentes(Arrays.asList(route5, route6));
+        lieu6.setRoutesAdjacentes(Arrays.asList(route4, route5, route6));
+        destination.setRoutesAdjacentes(Arrays.asList(route6));
+
+        Carte carte = new Carte(Arrays.asList(lieu1, lieu2, lieu3, lieu4, lieu5, lieu6, destination));
+
+        Marcheur marcheur = new Marcheur("Test Marcheur", lieu1, destination);
+
+        Lieu initialPosition = marcheur.getPositionActuelle();
+
+        marcheur.marcher(carte);
+
+        assertEquals(lieu1, initialPosition);
+    }
 }
